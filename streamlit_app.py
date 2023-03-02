94116498bbe1b93ec5fac4108673444536125746
ricardvalc <111350442+ricardvalc@users.noreply.github.com>
2023-03-02 12:48:23 +0100

    Update streamlit_app.py
    updated breakfast menu with emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Tuna melt sandwich')
-streamlit.text('Pain au chocolat')
-streamlit.text('Avocado and egg toast')
+streamlit.header('Breakfast Favorites)
+streamlit.text('Tuna melt sandwich 🐟🧀 ')
+streamlit.text('Pain au chocolat🥐🍫')
+streamlit.text('Avocado and egg toast 🥑🍞🍳')
 
