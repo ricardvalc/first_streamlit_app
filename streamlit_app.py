1c47f302be68cda20ea9ba0404b3bed7bfdd3d7c
ricardvalc <111350442+ricardvalc@users.noreply.github.com>
2023-03-02 12:48:49 +0100

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Favorites)
+streamlit.header('Breakfast Favorites')
 streamlit.text('Tuna melt sandwich 🐟🧀 ')
 streamlit.text('Pain au chocolat🥐🍫')
 streamlit.text('Avocado and egg toast 🥑🍞🍳')
