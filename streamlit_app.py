6c97e27ca7c6f22abf79311617e084073d350d81
ricardvalc <111350442+ricardvalc@users.noreply.github.com>
2023-03-02 12:54:46 +0100

    Update streamlit_app.py
    added pandas and csv with fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,7 @@ streamlit.text('Pain au chocolat🥐🍫')
 streamlit.text('Avocado and egg toast 🥑🍞🍳')
 
 streamlit.header('Make your own frickin smoothie🍔🍇🍊')
+
+import pandas as pd
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
